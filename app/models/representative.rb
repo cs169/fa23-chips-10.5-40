c46bb7958c63442882d677c242e0b283237ae094
Irene Lee <irene.lee@berkeley.edu>
2023-11-21 06:15:08 +0000

    Fixed legacy code_before testing

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,6 +9,11 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      street = ''
+      city = ''
+      state = ''
+      zip = ''
+      political_party = ''
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
@@ -16,12 +21,28 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
+      if official.address
+        address = official.address[0]
+        street = address.line1
+        city = address.city
+        state = address.state
+        zip = address.zip
+      end
+      political_party = official.party if official.party
+      next if Representative.exists?(name: official.name, ocdid: ocdid_temp)
 
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+      rep = Representative.create!({
+                                     name:            official.name,
+                                     ocdid:           ocdid_temp,
+                                     title:           title_temp,
+                                     street:          street,
+                                     city:            city,
+                                     state:           state,
+                                     zip:             zip,
+                                     political_party: political_party
+                                   })
       reps.push(rep)
     end
-
     reps
   end
 end
