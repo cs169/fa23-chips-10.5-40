1a1333b5582705727c1705109eeac11e728076df
Paul Yi <ipaule@berkeley.edu>
2023-12-05 17:07:26 +0000

    task 2.3 &2.4

--- app/models/campaign_finance.rb
+++ app/models/campaign_finance.rb
@@ -1,28 +1,46 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 class CampaignFinance < ApplicationRecord
+  has_many :news_items, dependent: :delete_all
 
-  def self.propublica_api_to_campaign_finance_params(results)
+  def self.categories
+    {
+      'candidate-loan'      => 'candidate_loans',
+      'contribution-total'  => 'total_contributions',
+      'debts-owed'          => 'debts_owed',
+      'disbursements-total' => 'total_disbursements',
+      'end-cash'            => 'end_cash',
+      'individual-total'    => 'total_from_individuals',
+      'pac-total'           => 'total_from_pacs',
+      'receipts-total'      => 'total_contributions',
+      'refund-total'        => 'total_refunds'
+    }
+  end
+
+  def self.cycles
+    (2010..2020).step(2).to_a
+  end
+
+  def self.propublica_api_to_campaign_finance_params(results, cycle, category)
     campaign_finance_entries = []
+    category_key = categories[category] || category
     results.each do |result|
-      name = result['name'].split(', ')
-      first_name = name.last
-      last_name = name.first
-      entry = CampaignFinance.find_or_initialize_by(first_name: first_name, last_name: last_name)
-      #where is receipts total? can't find it in the api response example
-      entry.assign_attributes(
-        candidate_loans: result['candidate_loans'],
-        total_contributions: result['total_contributions'],
-        debts_owed: result['debts_owed'],
-        total_disbursements: result['total_disbursements'],
-        end_cash: result['end_cash'],
-        total_from_individuals: result['total_from_individuals'],
-        total_from_pacs: result['total_from_pacs'],
-        total_refunds: result['total_refunds'],
-      )
+      name = result['name']
+      category_value = result[category_key]
+      entry = CampaignFinance.new(name: name, cycle: cycle, category: category_value)
       entry.save!
       campaign_finance_entries.push(entry)
     end
     campaign_finance_entries
+  end
+end
+
+  def self.find_from_top_twenty(search_params)
+    key = '9lcjslvwVjbqtX0KcQQ3W9rFm316caQQ2T89n4xA'
+    url = "https://api.propublica.org/campaign-finance/v1/#{search_params[:cycle]}/candidates/leaders/#{categories[search_params[:category]]}"
+    response = Faraday.get(url) do |request|
+      request.headers['X-API-Key'] = key
+    end
+    JSON.parse(response.body)['results'].map { |result| result['candidate'] }
   end
 end
