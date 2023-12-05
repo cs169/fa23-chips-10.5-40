b53355bc17943ddaa313d32b6d7bce3be838ad65
Paul Yi <ipaule@berkeley.edu>
2023-12-05 17:22:57 +0000

    spec fixed

--- spec/models/campaign_finance_spec.rb
+++ spec/models/campaign_finance_spec.rb
@@ -1,40 +1,29 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe CampaignFinance, type: :model do
+
+describe CampaignFinance do
   describe '.propublica_api_to_campaign_finance_params' do
-    let(:api_results) do
-      {
-        'status' => 'OK',
-        'results' => [
-          {
-          'name' => 'DOE, JOHN',
-          'candidate_loans' => 1000.0,
-          'total_contributions' => 2000.0,
-          'debts_owed' => 500.0,
-          'total_disbursements' => 1500.0,
-          'end_cash' => 2500.0,
-          'total_from_individuals' => 1750.0,
-          'total_from_pacs' => 250.0,
-          'total_refunds' => 100.0,
-        }
-        ] 
-      }
+    before do
+      @rep_info = double('rep_info')
     end
 
-    it 'creates or updates campaign finance records' do
-      expect { CampaignFinance.propublica_api_to_campaign_finance_params(api_results['results'], '2020', 'candidate-loan') }
-        .to change { CampaignFinance.count }.by(api_results['results'].size)
-    end
+    it 'creates campaign finance entries from rep_info results' do
+      allow(@rep_info).to receive(:results).and_return([
+        { 'name' => 'GARDNER, CORY', 'total_from_individuals' => '3527890.7', 'total_from_pacs' => '1706367.74' },
+        { 'name' => 'PETERS, GARY', 'total_from_individuals' => '4908369.62', 'total_from_pacs' => '1505646.46' }
+      ])
 
-    it 'correctly assigns attributes to campaign finance records' do
-      CampaignFinance.propublica_api_to_campaign_finance_params(api_results['results'], '2020', 'candidate-loan')
-      record = CampaignFinance.first
+      expect { CampaignFinance.propublica_api_to_campaign_finance_params(@rep_info.results, '2020', 'individual-total') }
+        .to change { CampaignFinance.count }.by(2)
 
-      expect(record.first_name).to eq('JOHN')
-      expect(record.last_name).to eq('DOE')
-      expect(record.candidate_loans).to eq(1000.0)
+      record1 = CampaignFinance.find_by(name: 'GARDNER, CORY')
+      record2 = CampaignFinance.find_by(name: 'PETERS, GARY')
 
+      expect(record1.cycle).to eq('2020')
+      expect(record1.category).to eq('3527890.7')
+      expect(record2.cycle).to eq('2020')
+      expect(record2.category).to eq('4908369.62')
     end
   end
 end
