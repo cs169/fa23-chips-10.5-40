77716026b56333152b71576698e88692947101e7
Owen Hu <owenhu0410@berkeley.edu>
2023-12-06 23:54:22 +0000

    add tests for cmpaign finance

--- spec/models/campaign_finance_spec.rb
+++ spec/models/campaign_finance_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe CampaignFinance, type: :model do
   describe '.cycles' do
     it 'returns an array of years from 2010 to 2020' do
-      expect(described_class.cycles).to eq((2010..2020).to_a)
+      expect(described_class.cycles).to eq((2010..2020).select {|year| year.even?})
     end
   end
 
@@ -17,4 +17,35 @@ RSpec.describe CampaignFinance, type: :model do
       expect(described_class.categories.keys).to contain_exactly(*expected_keys)
     end
   end
+
+  describe '.find_from_top_twenty' do
+    let(:search_params) { { cycle: '2020', category: 'candidate-loan' } }
+    let(:response) { double('response') }
+
+    context 'when API call is successful' do
+      before do
+        allow(response).to receive(:success?).and_return(true)
+        allow(response).to receive(:body).and_return({ results: [{ name: 'John Doe', candidate_loans: 1000 }] }.to_json)
+        allow(Faraday).to receive(:get).and_return(response)
+      end
+
+      it 'returns the data from the API' do
+        results = CampaignFinance.find_from_top_twenty(search_params)
+        expect(results).to eq([{ 'name' => 'John Doe', 'candidate_loans' => 1000 }])
+      end
+    end
+
+    context 'when API call fails' do
+      before do
+        allow(response).to receive(:success?).and_return(false)
+        allow(response).to receive(:body).and_return('{}')
+        allow(Faraday).to receive(:get).and_return(response)
+      end
+
+      it 'returns an empty array' do
+        expect(CampaignFinance.find_from_top_twenty(search_params)).to eq([])
+      end
+    end
+  end
+  
 end
