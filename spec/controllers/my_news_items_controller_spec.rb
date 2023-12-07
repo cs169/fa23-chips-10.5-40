18e60ac69b8c4f8651b098bb7f077a572f38392c
Angelina Songco <angelinasongco@berkeley.edu>
2023-12-07 00:55:12 +0000

    added tests for my_news_items

--- spec/controllers/my_news_items_controller_spec.rb
+++ spec/controllers/my_news_items_controller_spec.rb
@@ -1,8 +1,112 @@
 # frozen_string_literal: true
 
-# spec/controllers/my_news_items_controller_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe MyNewsItemsController, type: :controller do
-  
+  let(:user) do
+    User.create!(
+      first_name: 'John',
+      last_name: 'Doe',
+      uid: 'UID1',
+      provider: :google_oauth2
+    )
+  end
+
+  let(:representative) do
+    Representative.create!(
+      name: 'John Doe',
+      ocdid: 'ocd-division/country:us/state:example',
+      title: 'Position Title',
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'State',
+      zip: '12345',
+    )
+  end
+
+  let(:valid_attributes) do
+    {
+      title: 'Sample Title',
+      description: 'Sample Description',
+      link: 'https://example.com',
+      representative_id: representative.id,
+      issue: 'Sample Issue'
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      title: nil,
+      description: 'Sample Description',
+      link: 'https://example.com',
+      representative_id: representative.id,
+      issue: 'Sample Issue'
+    }
+  end
+
+  before do
+    session[:current_user_id] = user.id
+  end
+
+  describe 'GET #new' do
+    it 'assigns a new news_item as @news_item' do
+      get :new, params: { representative_id: representative.id }
+      expect(assigns(:news_item)).to be_a_new(NewsItem)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new NewsItem' do
+        expect do
+          post :create, params: { representative_id: representative.id, news_item: valid_attributes }
+        end.to change(NewsItem, :count).by(1)
+      end
+
+      it 'assigns a newly created news_item as @news_item' do
+        post :create, params: { representative_id: representative.id, news_item: valid_attributes }
+        expect(assigns(:news_item)).to be_a(NewsItem)
+        expect(assigns(:news_item)).to be_persisted
+      end
+
+      it 'redirects to the created news_item' do
+        post :create, params: { representative_id: representative.id, news_item: valid_attributes }
+        expect(response).to redirect_to(representative_news_item_path(representative, assigns(:news_item)))
+      end
+    end
+
+    context 'with invalid params' do
+      it 're-renders the "new" template' do
+        post :create, params: { representative_id: representative.id, news_item: invalid_attributes }
+        expect(response).to redirect_to(representative_news_item_path(representative, assigns(:news_item)))
+      end
+    end    
+  end
+
+  describe 'PUT #update' do
+    let!(:news_item) do
+      NewsItem.create!(
+        valid_attributes.merge(representative: representative)
+      )
+    end
+
+    context 'with valid params' do
+      it 'updates the requested news_item' do
+        new_attributes = { title: 'Updated Title' }
+        put :update, params: { id: news_item.id, news_item: new_attributes, representative_id: representative.id }
+        news_item.reload
+        expect(news_item.title).to eq('Updated Title')
+      end
+
+      it 'assigns the requested news_item as @news_item' do
+        put :update, params: { id: news_item.id, news_item: valid_attributes, representative_id: representative.id }
+        expect(assigns(:news_item)).to eq(news_item)
+      end
+
+      it 'redirects to the news_item' do
+        put :update, params: { id: news_item.id, news_item: valid_attributes, representative_id: representative.id }
+        expect(response).to redirect_to(representative_news_item_path(representative, news_item))
+      end
+    end   
+  end
+end
